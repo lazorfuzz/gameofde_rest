4bb87cac9c9350e8aabbdc95984eade2d2b17c96
Georgy <gob5081@psu.edu>
2019-07-01 20:17:02 -0400

    deleted duplicate inside the branch

--- gameofde_rest-master/cipher_helper.py
+++
@@ -1,7 +0,0 @@
-from CaesarDecipher import decrypt
-
-
-# The CaesarController calls this function in models.mainControllers.CaesarController
-def decipher(cipher, lang):
-    encryptedMessage = decrypt(cipher, lang)
-    return encryptedMessage
