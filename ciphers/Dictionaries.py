de1b9e721ce5d4fb80c2ea7a46a6cb967df5af54
lazorfuzz <leontosy@gmail.com>
2019-07-06 02:50:44 -0700

    Refactor langauge selection to use lang codes

--- ciphers/Dictionaries.py
+++ ciphers/Dictionaries.py
@@ -1,38 +1,29 @@
 # TOP 10 SPOKEN LANGUAGES IN US
 readfile =""
 
-def languageSelection(language):
-    if language == "arabic":
-        readfile = "Dictionary/Arabic.txt"
-    elif language == "chinese":
-        readfile = "Dictionary/Chinese.txt"
-    elif language == "english":
-        readfile = "Dictionary/English.txt"
-    elif language == "french":
-        readfile = "Dictionary/French.txt"
-    elif language == "german":
-        readfile = "Dictionary/German.txt"
-    elif language == "italian":
-        readfile = "Dictionary/Italian.txt"
-    elif language == "korean":
-        readfile = "Dictionary/Korean.txt"
-    elif language == "russian":
-        readfile = "Dictionary/Russian.txt"
-    elif language == "spanish":
-        readfile = "Dictionary/Spanish.txt"
-    elif language == "tagalog":
-        readfile = "Dictionary/Tagalog.txt"
-    elif language == "vietnamese":
-        readfile = "Dictionary/Vietnamese.txt"
-    return readfile
+lang_files = {
+    'ar': 'Dictionary/Arabic.txt',
+    'zh': 'Dictionary/Chinese.txt',
+    'en': 'Dictionary/English.txt',
+    'fr': 'Dictionary/French.txt',
+    'de': 'Dictionary/German.txt',
+    'it': 'Dictionary/Italian.txt',
+    'ko': 'Dictionary/Korean.txt',
+    'ru': 'Dictionary/Russian.txt',
+    'es': 'Dictionary/Spanish.txt',
+    'tl': 'Dictionary/Tagalog.txt',
+    'vi': 'Dictionary/Vietnamese.txt'
+}
 
+def languageSelection(language):
+    return lang_files.get(language)
 
 def dictionarylookup(language, word):
     readfile = languageSelection(language)
     if readfile != "":
-        file = open(readfile, encoding="utf-16")
-        for line in file:
-            if word.strip() == line.strip():
-                return True
-                break
+        with open(readfile, encoding="utf-16") as file:
+            for line in file:
+                if word.strip() == line.strip():
+                    return True
+                    break
     return False
