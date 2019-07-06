f1c0b3fa21bf58ccfb5c88e3f07dcc98950795be
lazorfuzz <leontosy@gmail.com>
2019-07-05 23:24:55 -0700

    Add solution and saved_solution models

--- controllers/mainControllers.py
+++ controllers/mainControllers.py
@@ -1,14 +1,18 @@
 from flask_restful import Resource, reqparse
 from database import db
-from models import User, AuthToken, Organization
+from models import User, AuthToken, Organization, Solution, SavedSolution
 from ciphers.cipher_helper import decipher
 from functools import wraps
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import base64
 
 parser = reqparse.RequestParser(bundle_errors=True)
 parser.add_argument('cipher')
 parser.add_argument('lang')
+parser.add_argument('solution')
+parser.add_argument('org_id')
+parser.add_argument('user_id')
 parser.add_argument('name')
 parser.add_argument('Authorization', location='headers')
 
@@ -16,6 +20,9 @@ def this_user():
   token = parser.parse_args()['Authorization']
   user = User.query.filter_by(id=token.user_id).first()
   return user
+
+def generic_400():
+  return {'status': 'error', 'message': 'Could not understand request'}, 400
 
 def authenticate(func):
   @wraps(func)
@@ -34,8 +41,17 @@ class CaesarController(Resource):
   # Handle cipher entry
   def post(self):
     args = parser.parse_args()
-    print('CIPHER', args['cipher'])
-    return {'result': decipher(args['cipher'], args['lang'])}
+    try:
+      cipher = base64.b64decode(args['cipher'])
+      solution = Solution.query.filter_by(cipher=cipher).first()
+      if solution:
+        return {'result': solution.solution}
+      new_solution = Solution(cipher, args['lang'], args['solution'], args['user_id'], args['org_id'])
+      db.session.add(new_solution)
+      db.session.commit()
+      return {'result': decipher(cipher, args['lang'])}
+    except:
+      return generic_400()
   
 class OrganizationController(Resource):
   method_decorators = [authenticate]
@@ -54,8 +70,7 @@ class OrganizationController(Resource):
     db.session.add(new_org)
     db.session.commit()
     # Make user the admin of the new organization
-    token = AuthToken.query.filter_by(data=args['Authorization']).first()
-    req_user = User.query.filter_by(id=token.user_id).first()
+    req_user = this_user()
     new_org_id = Organization.query.filter_by(name=org_name).first().id
     req_user.org_id = new_org_id
     req_user.role = 'admin'
@@ -65,8 +80,7 @@ class OrganizationController(Resource):
   def put(self, org_name):
     args = parser.parse_args()
     org = Organization.query.filter_by(name=org_name).first_or_404()
-    token = AuthToken.query.filter_by(data=args['Authorization']).first()
-    req_user = User.query.filter_by(id=token.user_id).first()
+    req_user = this_user()
     # Only allow update if the user is an admin of the organization
     if req_user.role == 'admin' and int(req_user.org_id) == int(org.id):
       if args['name']: org.name = args['name']
@@ -91,3 +105,39 @@ class NewsController(Resource):
       return list(map(lambda n: {'title': n.title.text, 'link': n.link.text, 'date': n.pubDate.text}, news_list))
     except:
       return {'status': 'error', 'message': 'News not found!'}, 404
+
+class CheckSolutionController(Resource):
+  method_decorators = [authenticate]
+  def post(self):
+    try:
+      args = parser.parse_args()
+      cipher = args['cipher']
+      solution = Solution.query.filter_by(cipher=cipher).first_or_404()
+      return {'id': solution.id, 'cipher': solution.cipher, 'lang': solution.lang, 'solution': solution.solution, user_id: 'solution.user_id', org_id: 'solution.org_id'}
+    except:
+      return generic_400()
+
+class SavedSolutionsController(Resource):
+  method_decorators = [authenticate]
+  def get(self):
+    try:
+      current_user = this_user()
+      saved_solutions = SavedSolution.query.filter_by(user_id=current_user.id).all()
+      return list(map(lambda s: {'id': solution.id, 'cipher': solution.cipher, 'lang': solution.lang, 'solution': solution.solution, user_id: 'solution.user_id'}, saved_solutions))
+    except:
+      return generic_400()
+  
+  def post(self):
+    try:
+      args = parser.parse_args()
+      # Check if saved solution already exists
+      existing = SavedSolution.query.filter_by(cipher=args['cipher']).first()
+      if existing:
+        return {'status': 'error', 'message': 'Solution already saved!'}, 401
+      # Create new saved solution
+      new_saved_solution = SavedSolution(args['cipher'], args['lang'], args['solution'], args['user_id'])
+      db.session.add(new_saved_solution)
+      db.session.commit()
+      return {'status': 'success'}, 201
+    except:
+      return generic_400()
--- models.py
+++ models.py
@@ -15,7 +15,7 @@ class User(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True)
-    role = db.Column(db.String(120))
+    role = db.Column(db.String(80))
     org_id = db.Column(db.Integer, db.ForeignKey('organization.id'))
 
     def __init__(self, username, password, email, role, org_id):
@@ -31,7 +31,7 @@ class User(db.Model):
 class AuthToken(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    data = db.Column(db.String(80), unique=True, nullable=False)
+    data = db.Column(db.String(64), unique=True, nullable=False)
 
     def __init__(self, user, data):
         self.user_id = user.id
@@ -40,7 +40,39 @@ class AuthToken(db.Model):
     def __repr__(self):
         return '<AuthToken %r>' % self.data
 
+class Solution(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    org_id = db.Column(db.Integer, db.ForeignKey('organization.id'))
+    cipher = db.Column(db.String(100000), nullable=False)
+    lang = db.Column(db.String(2), nullable=False)
+    solution = db.Column(db.String(100000), nullable=False)
 
+    def __init__(self, cipher, lang, solution, user_id, org_id):
+        self.cipher = cipher
+        self.lang = lang
+        self.solution = solution
+        self.user_id = user_id
+        self.org_id = org_id
+    
+    def __repr__(self):
+        return '<Solution %d>' % self.id
+
+class SavedSolution(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    cipher = db.Column(db.String(100000), nullable=False)
+    lang = db.Column(db.String(2), nullable=False)
+    solution = db.Column(db.String(100000), nullable=False)
+
+    def __init__(self, cipher, lang, solution, user_id):
+        self.cipher = cipher
+        self.lang = lang
+        self.solution = solution
+        self.user_id = user_id
+    
+    def __repr__(self):
+        return '<SavedSolution %d>' % self.id
 
 if __name__ == '__main__':
     db.create_all()
